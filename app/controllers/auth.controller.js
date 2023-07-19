8deef9ed9a2550d7a6ca4ebb5a1aacd2f46a1aff
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-07-19 19:11:15 +0200

    change registration flow

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,33 +1,43 @@
+const CustomError = require('../../utils/CustomError');
+const User = require('../models/user');
 const AuthService = require('../services/auth.service');
 
 class AuthController {
-	constructor() {
-		this.authService = new AuthService();
-	}
+    constructor() {
+        this.authService = new AuthService();
+        this.user = new User();
+    }
 
-	// Method to register a new user
-	register = async (req, res, next) => {
-		try {
-			const data = req.body;
-			const result = await this.authService.register(data);
-			res.status(201).json(result);
-		} catch (error) {
-			console.error('Error registering user:', error);
-			next(error);
-		}
-	};
+    // Method to register a new user
+    register = async(req, res, next) => {
+        try {
+            // Validate if user is admin
+            const user = req.user;
+            const admin = await this.user.findUserByEmail(user.email);
+            console.log(admin);
+            if (admin.role !== 'admin') throw new CustomError('Unauthorized', 401);
 
-	// Method to handle user login
-	login = async (req, res, next) => {
-		try {
-			const data = req.body;
-			const result = await this.authService.login(data);
-			res.status(200).json(result);
-		} catch (error) {
-			console.error('Error logging in user:', error);
-			next(error);
-		}
-	};
+            // Register user
+            const data = req.body;
+            const result = await this.authService.register(data);
+            res.status(201).json(result);
+        } catch (error) {
+            console.error('Error registering user:', error);
+            next(error);
+        }
+    };
+
+    // Method to handle user login
+    login = async(req, res, next) => {
+        try {
+            const data = req.body;
+            const result = await this.authService.login(data);
+            res.status(200).json(result);
+        } catch (error) {
+            console.error('Error logging in user:', error);
+            next(error);
+        }
+    };
 }
 
 module.exports = AuthController;
--- database/seed.js
+++ database/seed.js
@@ -2,44 +2,49 @@ const bcrypt = require('bcryptjs');
 const pool = require('../config/db');
 
 class Seeder {
-	seedData = async (tableName, data) => {
-		const client = await pool.connect();
-		try {
-			await client.query('BEGIN');
+    seedData = async(tableName, data) => {
+        const client = await pool.connect();
+        try {
+            await client.query('BEGIN');
 
-			for (const item of data) {
-				const keys = Object.keys(item);
-				const values = Object.values(item);
-				const placeholders = values.map((_, i) => `$${i + 1}`).join(', ');
+            for (const item of data) {
+                const keys = Object.keys(item);
+                const values = Object.values(item);
+                const placeholders = values.map((_, i) => `$${i + 1}`).join(', ');
 
-				const query = `INSERT INTO ${tableName} (${keys}) VALUES (${placeholders})`;
-				await client.query(query, values);
-			}
+                const query = `INSERT INTO ${tableName} (${keys}) VALUES (${placeholders})`;
+                await client.query(query, values);
+            }
 
-			await client.query('COMMIT');
+            await client.query('COMMIT');
 
-			console.log(`${tableName} seed completed successfully!`);
-		} catch (error) {
-			console.error(`Error during ${tableName} seed:`, error);
-		} finally {
-			client.release();
-		}
-	};
+            console.log(`${tableName} seed completed successfully!`);
+        } catch (error) {
+            console.error(`Error during ${tableName} seed:`, error);
+        } finally {
+            client.release();
+        }
+    };
 
-	seedAll = async () => {
-		// Write your seed data here:
-		const userData = [
-			{
-				name: 'John Doe',
-				email: 'john@example.com',
-				password: bcrypt.hashSync('password', 10),
-				role: 'student',
-			},
-		];
+    seedAll = async() => {
+        // Write your seed data here:
+        const userData = [{
+                name: 'test',
+                email: 'test@example.com',
+                password: bcrypt.hashSync('test123', 10),
+                role: 'student',
+            },
+            {
+                name: 'admin',
+                email: 'admin@example.com',
+                password: bcrypt.hashSync('admin123', 10),
+                role: 'admin',
+            },
+        ];
 
-		// Call the seedData method here:
-		await this.seedData('users', userData);
-	};
+        // Call the seedData method here:
+        await this.seedData('users', userData);
+    };
 }
 
 const seeder = new Seeder();
