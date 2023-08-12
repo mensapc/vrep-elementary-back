01ebfee97d4e7eec1300e5d7104232ee19d81d93
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-08-12 16:27:46 +0200

    Chore: add error response middleware

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const errorMiddleware = require('./middlewares/error.middleware');
 require('dotenv').config();
 const admin = require('firebase-admin');
 const serviceAccount = require('./config/serviceAccountKey.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 const PORT = process.env.PORT || 8080;
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to the RPMS API');
