06cd4987fe6f0d7fc20df0d7d420f5c57f6956d3
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-09-05 00:21:08 +0200

    Feat: Add exam date validation & converter

--- controllers/exam.controller.js
+++ controllers/exam.controller.js
@@ -1,7 +1,13 @@
 const { Query } = require("firefose");
 const Exam = require("../models/exam");
 const QuestionController = require("./question.controller");
-const { calculateMarks } = require("../utils/utils.exam");
+const {
+  calculateMarks,
+  checkExamAvailability,
+  examDuration,
+  validateExamDuration,
+} = require("../utils/utils.exam");
+const CustomError = require("../utils/CustomError");
 
 class ExamController {
   constructor() {
@@ -11,6 +17,15 @@ class ExamController {
     const examData = req.body;
 
     try {
+      // validate exam duration
+      const durationValidate = validateExamDuration(examData);
+      if (!durationValidate.is_valid) throw new CustomError(durationValidate.message, 400);
+
+      // calculate exam duration
+      const duration = examDuration(examData);
+      if (!duration.is_valid) throw new CustomError(duration.message, 400);
+      examData.time_limit = duration.duration;
+
       const newExam = await Exam.create({ ...examData, staff_id: req.user.staff_id });
       res.status(200).json({ exam: newExam });
     } catch (error) {
@@ -34,6 +49,10 @@ class ExamController {
     const { exam_id } = req.params;
     try {
       const exam = await Exam.findById(exam_id);
+      // check if exam is available
+      const examAvailability = checkExamAvailability(exam);
+      if (!examAvailability.is_available) throw new CustomError(examAvailability.message, 403);
+
       const questionsWithOptions = await this.questionController.examQuestionsWithOptions(exam.id);
       res.status(200).json({ exam: { ...exam, questions: questionsWithOptions } });
     } catch (error) {
--- utils/utils.exam.js
+++ utils/utils.exam.js
@@ -11,4 +11,69 @@ const calculateMarks = (exam, questionsWithAnswers) => {
   return (studentMarks / totalPoints) * exam.max_score;
 };
 
-module.exports = { calculateMarks };
+const ExamTimeInMilliseconds = (exam) => {
+  const currentTime = new Date();
+  const examStartTime = new Date(exam.start_date);
+  const examEndTime = new Date(exam.end_date);
+  return { currentTime, examStartTime, examEndTime };
+};
+
+const timeConverter = (time) => {
+  const days = Math.floor(time / 86400000);
+  const hours = Math.floor((time % 86400000) / 3600000);
+  const minutes = Math.floor((time % 3600000) / 60000);
+  const seconds = Math.floor((time % 60000) / 1000);
+  return { days, hours, minutes, seconds };
+};
+
+const validateExamDuration = (exam) => {
+  const { start_date, end_date } = exam;
+  if (!start_date || !end_date) {
+    return { is_valid: false, message: "Exam start time and exam end time must be provided." };
+  }
+
+  const { examStartTime, examEndTime, currentTime } = ExamTimeInMilliseconds(exam);
+
+  if (examStartTime < currentTime || examEndTime < currentTime) {
+    return { is_valid: false, message: "Exam start time and exam end time must be in the future." };
+  }
+
+  if (examStartTime >= examEndTime) {
+    return { is_valid: false, message: "Exam start time must be before exam end time." };
+  }
+  return { is_valid: true };
+};
+
+const examDuration = (exam) => {
+  const { examStartTime, examEndTime } = ExamTimeInMilliseconds(exam);
+
+  const { hours, minutes, seconds } = timeConverter(examEndTime - examStartTime);
+  let duration = "";
+  if (hours > 0) duration += `${hours} hr `;
+  if (minutes > 0) duration += `${minutes} min `;
+  if (seconds > 0) duration += `${seconds} sec`;
+
+  return { is_valid: true, duration: duration.trim() };
+};
+
+const checkExamAvailability = (exam) => {
+  const { currentTime, examStartTime, examEndTime } = ExamTimeInMilliseconds(exam);
+  if (currentTime < examStartTime) {
+    const { days, hours, minutes, seconds } = timeConverter(examStartTime - currentTime);
+
+    let message = "";
+
+    if (days > 1) message += `${days} days `;
+    if (hours > 0 && hours < 24) message += `${hours} hours `;
+    if (minutes > 0) message += `${minutes} minutes `;
+    if (seconds > 0) message += `${seconds} seconds `;
+
+    return { is_available: false, message: `The exam will be available in ${message.trim()}.` };
+  } else if (currentTime > examEndTime) {
+    return { is_available: false, message: "The exam is no longer available." };
+  } else {
+    return { is_available: true };
+  }
+};
+
+module.exports = { calculateMarks, checkExamAvailability, examDuration, validateExamDuration };
