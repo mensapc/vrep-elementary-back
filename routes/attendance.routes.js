c25418b701980b658b6cbbe4588b514e607bbfe6
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-09-15 00:18:23 +0200

    Chore: Add attendance routes

---
+++ routes/attendance.routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { validateToken } = require("../middlewares/validations");
+const { authorize } = require("../middlewares/authorize");
+const AttendanceController = require("../controllers/attendance.controller");
+
+const router = express.Router();
+const attendanceController = new AttendanceController();
+
+router.post(
+  "/attendance",
+  validateToken,
+  authorize(["createAttendance"]),
+  attendanceController.createAttendance
+);
+router.get(
+  "/attendance/students/:student_id",
+  validateToken,
+  attendanceController.getAttendanceByStudent
+);
+router.get(
+  "/attendance/courses/:course_id",
+  validateToken,
+  attendanceController.getAttendanceByCourse
+);
+
+module.exports = router;
