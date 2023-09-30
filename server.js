10a3839d8c151469bb2c4089cb44c2e8beabe6e3
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-09-30 13:54:21 +0200

    add secret file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const errorMiddleware = require("./middlewares/error.middleware");
 require("dotenv").config();
 const admin = require("firebase-admin");
-const serviceAccount = require("./config/serviceAccountkey.json");
+const serviceAccount = require("./serviceAccountkey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
