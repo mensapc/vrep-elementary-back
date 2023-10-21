4e42fe6347429340e5c866370487d217bf2c79be
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-10-21 23:19:27 +0200

    Chore: Add independent student login API

--- routes/students.authroutes.js
+++ routes/students.authroutes.js
@@ -1,15 +1,36 @@
-const express = require('express');
-const StudentAuthController = require('../controllers/student.controller');
-const { authorize } = require('../middlewares/authorize');
-const { validateToken } = require('../middlewares/validations');
+const express = require("express");
+const StudentAuthController = require("../controllers/student.controller");
+const { authorize } = require("../middlewares/authorize");
+const { validateToken } = require("../middlewares/validations");
 const router = express.Router();
 const StudentController = new StudentAuthController();
 
+router.post(
+  "/register-student/:userType",
+  validateToken,
+  authorize(["createStudent1"]),
+  StudentController.addStudent
+);
+router.get(
+  "/single/student",
+  validateToken,
+  authorize(["readStudent"]),
+  StudentController.getSingleStudent
+);
+router.get(
+  "/students",
+  validateToken,
+  authorize(["readStudents"]),
+  StudentController.getAllStudents
+);
+router.delete(
+  "/delete/student/:reg_number",
+  validateToken,
+  authorize(["deleteStudent"]),
+  StudentController.deleteStudent
+);
+router.put("/update", validateToken, authorize(["updateStudent"]), StudentController.updateStudent);
 
-router.post('/register-student/:userType', validateToken, authorize(['createStudent1']), StudentController.addStudent);
-router.get('/single/student', validateToken, authorize(['readStudent']), StudentController.getSingleStudent);
-router.get('/students', validateToken, authorize(['readStudents']), StudentController.getAllStudents)
-router.delete('/delete/student/:reg_number', validateToken, authorize(['deleteStudent']), StudentController.deleteStudent);
-router.put('/update', validateToken, authorize(['updateStudent']), StudentController.updateStudent)
+router.post("/login/student", StudentController.login);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const serviceAccount = require("./serviceAccountkey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-const authRoutes = require("./routes/auth.routes");
+// const authRoutes = require("./routes/auth.routes");
 const studentRoutes = require("./routes/students.authroutes");
 const staffRoutes = require("./routes/staff.routes");
 const courseRoutes = require("./routes/course.route");
@@ -27,7 +27,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
-app.use("/", authRoutes);
+// app.use("/", authRoutes);
 app.use("/", studentRoutes);
 app.use("/", staffRoutes);
 app.use("/", courseRoutes);
