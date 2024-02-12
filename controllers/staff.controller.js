76d80710773597d67eb22fc92293e6a6199b09cb
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-02-12 22:47:29 +0200

    change controllers

---
+++ controllers/staff.controller.js
@@ -0,0 +1,94 @@
+const Staff = require('../models/staff');
+const CustomError = require('../utils/CustomError');
+const generateToken = require('../utils/utils.token');
+const BcryptPassword = require('../utils/utils.bcrypt.password');
+const registrationUtils = require('../utils/utils.registration');
+
+class StaffController {
+	constructor() {
+		this.bcryptPassword = new BcryptPassword();
+		this.registrationUtils = new registrationUtils();
+	}
+
+	register = async (req, res, next) => {
+		const userData = req.body;
+
+		try {
+			this.registrationUtils.validateData(userData, 'staff');
+			const staff = await Staff.findOne({ email: userData.email });
+			if (staff) throw new CustomError('staff already exists', 400);
+			const hashedPassword = await this.bcryptPassword.HashPassword(userData.password);
+
+			const newStaff = await Staff.create({ ...userData, password: hashedPassword });
+			delete newStaff._doc.password;
+
+			const token = generateToken({ id: newStaff._id, email: newStaff.email, role: newStaff.role });
+			res.status(200).json({ ...newStaff._doc, token });
+		} catch (error) {
+			console.error(`Error registering staff: ${error}`);
+			next(error);
+		}
+	};
+
+	login = async (req, res, next) => {};
+
+	//get all staff
+	findAllStaff = async (req, res, next) => {
+		try {
+			const staff = await this.staff.getAllStaff();
+
+			res.status(200).json({ Allstaff: staff });
+		} catch (error) {
+			console.error(`Error retrieving all staff's `, error);
+			next(error);
+		}
+	};
+	// get satff by id
+	getStaffById = async (req, res, next) => {
+		const staffId = req.params.staff_id; // Use staff_id from req.params
+
+		try {
+			const staff = await this.staff.getStaffById(staffId);
+			res.status(200).json(staff);
+		} catch (error) {
+			console.error('Error in getStaffById controller:', error);
+			res.status(error.statusCode || 500).json({ error: error.message });
+		}
+	};
+	// deleting staff by id
+	deleteStaffById = async (req, res, next) => {
+		const staff_id = req.params.staff_id; // Extract staff ID from req.body
+		try {
+			await this.staff.deleteStaffById(staff_id);
+			res.status(204).send(); // Successful deletion, no content to send
+		} catch (error) {
+			console.error('Error in deleteStaffById controller:', error);
+			res.status(error.statusCode || 500).json({ error: error.message });
+		}
+	};
+	// updating staff details by id
+	updateStaffById = async (req, res, next) => {
+		try {
+			const staff_id = req.params.staff_id;
+			const updatedData = req.body; // Assuming you send the entire updated data in the request body
+
+			if (!staff_id) {
+				throw new CustomError('Staff ID is required', 400);
+			}
+
+			const updatedStaff = await this.staff.updateStaffById(staff_id, updatedData);
+
+			if (updatedStaff) {
+				res.status(201).json({ staff: updatedStaff });
+			} else {
+				throw new CustomError(`Staff with ID ${staff_id} not found`, 400);
+			}
+		} catch (error) {
+			console.error(`Error updating staff with ID ${req.params.staff_id}: ${error}`);
+			throw new Error('Failed to update staff.');
+		}
+	};
+}
+
+module.exports = StaffController;
+
