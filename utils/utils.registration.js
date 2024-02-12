c67e1bd1d4da2c169147da7a57abad40540af41e
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-02-12 19:52:19 +0200

    modify project utils

--- utils/utils.registration.js
+++ utils/utils.registration.js
@@ -8,11 +8,15 @@ class RegistrationUtils {
 	validateData = (data, userType) => {
 		const { email, password, role, first_name, last_name, age } = data;
 		try {
-			if (!email || !password || !role || !first_name || !last_name || !age) {
+			if (!email || !role || !first_name || !last_name || !age) {
 				throw new CustomError(
 					'First name, last name, age, email, role, and password are required',
 					400
 				);
+			}
+
+			if (role !== 'admin' && role !== 'staff' && role !== 'pupil') {
+				throw new CustomError('Invalid role', 400);
 			}
 
 			// Email format validation
@@ -21,25 +25,36 @@ class RegistrationUtils {
 				throw new CustomError('Invalid email format', 400);
 			}
 
-			// Password length validation
-			if (password.length < 8) {
-				throw new CustomError('Password must be at least 8 characters', 400);
-			}
-
 			if (first_name.length < 3 || last_name.length < 3) {
 				throw new CustomError('firt name or last name should be at least 3 characters');
 			}
 
-			if ((userType === 'staff' || userType === 'admin') && age < 18) {
-				throw new CustomError('User must be older than 18');
+			if (userType === 'staff' || userType === 'admin') {
+				if (!password) {
+					throw new CustomError('Password is required', 400);
+				}
+
+				if (password.length < 8) {
+					throw new CustomError('Password must be at least 8 characters', 400);
+				}
+
+				if (age < 18) {
+					throw new CustomError('User must be older than 18', 400);
+				}
 			}
 
-			if (user.role !== 'admin' || user.role !== 'staff' || user.role !== 'pupil') {
-				throw new CustomError('Invalid role', 400);
+			if (userType === 'pupil') {
+				const { address, health_condition, parent_name, parent_phone } = data;
+				if (!address || !health_condition || !parent_name || !parent_phone) {
+					throw new CustomError(
+						'Address, health condition, parent name, and parent phone are required',
+						400
+					);
+				}
 			}
 		} catch (error) {
 			if (error instanceof CustomError) throw error;
-			throw new Error('Registration failed');
+			throw new Error(`Registration failed:${error}`);
 		}
 	};
 
