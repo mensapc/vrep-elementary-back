2ec0be7167734b6e91e3ea1afa182ffcf5941953
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-02-13 17:11:16 +0200

    modify exam routes

--- routes/exam.routes.js
+++ routes/exam.routes.js
@@ -9,6 +9,7 @@ const examController = new ExamController();
 router.post('/exam', validateToken, authorize(['createExam']), examController.createExam);
 router.get('/exams', validateToken, authorize(['getExams']), examController.getExams);
 router.get('/exams/:id', validateToken, examController.getExam);
+router.get('/exams/:id/full', validateToken, examController.getExamWithQustionsAndOptions);
 router.get('/exams/:id/results/:student_id', validateToken, examController.getExamResults);
 router.put('/exams/:id', validateToken, authorize(['updateExam']), examController.updateExam);
 router.delete('/exams/:id', validateToken, authorize(['deleteExam']), examController.deleteExam);
