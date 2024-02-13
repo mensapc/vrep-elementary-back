8a35c61de7c50829da916065fcdadcdaf9ce6506
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-02-13 20:20:19 +0200

    modify role utils & staff model

--- models/staff.js
+++ models/staff.js
@@ -1,57 +1,56 @@
 const mongoose = require('mongoose');
 
 const staffSchema = new mongoose.Schema({
-	role: {
-		type: String,
-		required: true,
-		default: 'staff',
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
+  role: {
+    type: String,
+    required: true,
+    default: 'staff',
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-	},
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 
-	first_name: {
-		type: String,
-		required: true,
-		trim: true,
-	},
+  first_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 
-	last_name: {
-		type: String,
-		required: true,
-		trim: true,
-	},
+  last_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 
-	age: {
-		type: Number,
-		required: true,
-		trim: true,
-	},
+  age: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
 
-	phone_number: {
-		type: String,
-		required: true,
-		trim: true,
-	},
+  phone_number: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 
-	address: {
-		type: String,
-		required: true,
-		trim: true,
-	},
+  address: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 
-	photo: {
-		type: String,
-	},
-	allexams: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Exam' }],
+  photo: {
+    type: String,
+  },
 });
 
 const StaffModel = mongoose.model('Staff', staffSchema);
