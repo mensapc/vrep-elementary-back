990d7f11969d2272a19f5dc0f9d4a468fdce9d5e
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-02-13 21:12:20 +0200

    change route to create new answer

--- controllers/answer.controller.js
+++ controllers/answer.controller.js
@@ -1,19 +1,12 @@
-const { Query } = require("firefose");
-const Answer = require("../models/answer");
-const CustomError = require("../utils/CustomError");
+const Answer = require('../models/answer');
+const CustomError = require('../utils/CustomError');
 
 class AnswerController {
   createAnswer = async (req, res, next) => {
     const answerData = req.body;
-    if (!Array.isArray(answerData)) return res.status(400).send("Answer data must be an array");
-
     try {
-      let response = [];
-      for (let i = 0; i < answerData.length; i++) {
-        const newAnswer = await Answer.create(answerData[i]);
-        response.push(newAnswer);
-      }
-      res.status(200).json({ answer: response });
+      const answer = await Answer.create(answerData);
+      return res.status(200).json(answer);
     } catch (error) {
       console.error(`Error creating answer: ${error}`);
       next(error);
@@ -23,8 +16,8 @@ class AnswerController {
   studentAnswer = async (student_id, question_id) => {
     try {
       const query = new Query()
-        .where("student_id", "==", student_id)
-        .where("question_id", "==", question_id);
+        .where('student_id', '==', student_id)
+        .where('question_id', '==', question_id);
       const answer = await Answer.find(query);
       return answer;
     } catch (error) {
@@ -35,7 +28,7 @@ class AnswerController {
 
   deleteQuestionAnswers = async (question_id) => {
     try {
-      const query = new Query().where("question_id", "==", question_id);
+      const query = new Query().where('question_id', '==', question_id);
       const answers = await Answer.delete(query);
       return answers;
     } catch (error) {
