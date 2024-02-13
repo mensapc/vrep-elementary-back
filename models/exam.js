cabf3adfac675850fb511842486a529134473224
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-02-13 17:15:02 +0200

    modify exam model

--- models/exam.js
+++ models/exam.js
@@ -1,33 +1,37 @@
 const mongoose = require('mongoose');
 
 const examSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	description: {
-		type: String,
-		required: true,
-	},
-	max_score: {
-		type: Number,
-		required: true,
-	},
-	time_limit: {
-		type: String,
-		required: true,
-	},
-	start_date: {
-		type: String,
-		required: true,
-	},
-	end_date: {
-		type: String,
-		required: true,
-	},
-	creator: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff' },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  max_score: {
+    type: Number,
+    required: true,
+  },
+  time_limit: {
+    type: String,
+    required: true,
+  },
+  start_date: {
+    type: String,
+    required: true,
+  },
+  end_date: {
+    type: String,
+    required: true,
+  },
+  creator_id: {
+    type: String,
+    required: true,
+  },
+  questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
 });
 
-const ExamModel = mongoose.model('exams', examSchema);
+const ExamModel = mongoose.model('Exam', examSchema);
 
 module.exports = ExamModel;
