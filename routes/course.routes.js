85ca82ca5aa133de141592e4f1361fd5f57253c5
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-02-15 01:33:39 +0200

    modify courses routes

---
+++ routes/course.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { validateToken } = require('../middlewares/validations');
+const CourseController = require('../controllers/course.controller');
+const { authorize } = require('../middlewares/authorize');
+const courseController = new CourseController();
+const router = express.Router();
+
+router.post('/course', validateToken, authorize(['createCourse']), courseController.createCourse);
+// router.post('/create/course_schema', validateToken, authorize(['createCourseData']), courseController.createCourseSchemData);
+// router.get('/courses_scheme', validateToken, authorize(['getCoursesScheme']), courseController.getCourseSchemes)
+// router.get('/courses', validateToken, authorize(['readCourse']), courseController.getAllCourses);
+// router.get('/course/:courseId', validateToken, authorize(['readCourses']), courseController.getSingleCourse)
+// router.delete('/delete/course/:courseID', validateToken, authorize(['deletecourse']), courseController.deleteCourseByID)
+// router.put('/update/course/:courseID', validateToken, authorize(['updateCourse']), courseController.updateCourseByID)
+// router.put('/update/courses_scheme/:courseScheme_ID', validateToken, authorize(['updateScheme']), courseController.updateCourseScheme)
+
+module.exports = router;
+
