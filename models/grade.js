3af057b4f4384828c9a7be9edca9b4a3ea58cc5c
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-02-15 20:32:27 +0200

    modify grade model

--- models/grade.js
+++ models/grade.js
@@ -1,20 +1,15 @@
-const { Schema, Model, SchemaTypes } = require("firefose");
-const { String, Number } = SchemaTypes;
+const mongoose = require('mongoose');
 
-const gradeSchema = new Schema({
-  student_id: {
-    type: String,
-    required: true,
-  },
-  exam_id: {
-    type: String,
-    required: true,
-  },
-  grade_value: {
-    type: String,
-    required: true,
-  },
+const gradeSchema = new mongoose.Schema({
+  score: { type: Number, required: true },
+  total: { type: Number, required: true },
+  percentage: { type: Number, required: true },
+  grade: { type: String, default: '' },
+  student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
+  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
+  class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true },
+  exam: { type: mongoose.Schema.Types.ObjectId, ref: 'Exam', required: true },
 });
 
-const Grade = new Model("grades", gradeSchema);
-module.exports = Grade;
+const GradeModel = new Model('Grade', gradeSchema);
+module.exports = GradeModel;
