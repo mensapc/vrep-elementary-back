22e71138aea1e155900c0252a39850df89ad305f
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-02-16 02:36:01 +0200

    add controller to create exam

--- controllers/grade.controller.js
+++ controllers/grade.controller.js
@@ -1,15 +1,31 @@
-const { Query } = require("firefose");
-const Grade = require("../models/grade");
+const mongoose = require('mongoose');
+const Grade = require('../models/grade');
+const { ExamDetailsAndAnswers, CalculateResults } = require('../utils/utils.exam');
+const { checkGradeExistence } = require('../utils/utils.grade');
+const CustomError = require('../utils/CustomError');
 
 class GradeController {
   createGrade = async (req, res, next) => {
-    const gradeData = req.body;
+    const { exam_id, student_id } = req.body;
 
     try {
-      const newGrade = await Grade.create(gradeData);
-      res.status(200).json({ grade: newGrade });
+      const gradeExist = await checkGradeExistence(exam_id, student_id);
+      if (gradeExist) throw new CustomError('Grade already exists', 400);
+
+      const { exam, answers } = await ExamDetailsAndAnswers(exam_id, student_id, session);
+      const results = CalculateResults(exam, answers);
+      const grade = await Grade.create({
+        total: results.totalMarks,
+        percentage: results.percentage,
+        score: results.scoredMarks,
+        student: student_id,
+        course: results.course,
+        class: results.student_class,
+        exam: exam_id,
+      });
+      res.status(200).json(grade);
     } catch (error) {
-      console.error(`Error creating grade: ${error}`);
+      console.error(`Error calculating result: ${error}`);
       next(error);
     }
   };
@@ -17,7 +33,7 @@ class GradeController {
   getGradesByStudent = async (req, res, next) => {
     const { student_id } = req.params;
     try {
-      const query = new Query().where("student_id", "==", student_id);
+      const query = new Query().where('student_id', '==', student_id);
       const grades = await Grade.find(query);
       res.status(200).json({ grades });
     } catch (error) {
@@ -29,7 +45,7 @@ class GradeController {
   getGradesByExam = async (req, res, next) => {
     const { exam_id } = req.params;
     try {
-      const query = new Query().where("exam_id", "==", exam_id);
+      const query = new Query().where('exam_id', '==', exam_id);
       const grades = await Grade.find(query);
       res.status(200).json({ grades });
     } catch (error) {
