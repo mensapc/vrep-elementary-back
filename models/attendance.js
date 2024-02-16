a6d1b5f681cb4267847bf7b40c1dace089a129fb
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-02-16 03:31:15 +0200

    add attendance model

--- models/attendance.js
+++ models/attendance.js
@@ -1,24 +1,18 @@
-const { Schema, Model, SchemaTypes } = require("firefose");
-const { String, Number } = SchemaTypes;
+const mongoose = require('mongoose');
 
-const attendanceSchema = new Schema({
-  student_id: {
+const attendanceSchema = new mongoose.Schema({
+  student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
+  _class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true },
+  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
+  attendance_status: {
     type: String,
+    enum: ['Present', 'Absent', 'Tardy', 'Excused'],
     required: true,
   },
-  course_id: {
-    type: String,
-    required: true,
-  },
-  attendance_value: {
-    type: Number,
-    required: true,
-  },
-  total_attendance: {
-    type: Number,
-    required: true,
-  },
+  reason: { type: String },
+  comments: { type: String },
+  staff: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff', required: true },
 });
 
-const Attendance = new Model("attendances", attendanceSchema);
+const Attendance = mongoose.model('Attendance', attendanceSchema);
 module.exports = Attendance;
