6a4ce5a879ade81244c7ab55182ff4c4a141870c
Rwubakwanayo Olivier <rwubakwanayoolivier@gmail.com>
2024-02-21 01:08:51 +0200

    Update server.js
    Signed-off-by: Rwubakwanayo Olivier <rwubakwanayoolivier@gmail.com>

--- server.js
+++ server.js
@@ -28,9 +28,7 @@ app.use(
     origin: [
       'http://localhost:3000',
       'http://127.0.0.1:5500',
-      'https://rpsad.rulerspalacemontessori.com.ng',
-      'https://rpms-admin-m6mc55ooj-mpcircle.vercel.app'
-      
+      'https://rpms-admin.vercel.app/',
     ],
   })
 );
