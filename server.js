abb751a6a1b78f23cdd9f174058f17ac7be5a0d8
Rwubakwanayo Olivier <rwubakwanayoolivier@gmail.com>
2024-02-20 23:52:45 +0200

    Update server allowed client url
    Signed-off-by: Rwubakwanayo Olivier <rwubakwanayoolivier@gmail.com>

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ app.use(
     origin: [
       'http://localhost:3000',
       'http://127.0.0.1:5500',
-      'https://rpms-web-server.onrender.com',
+      'https://rpsad.rulerspalacemontessori.com.ng',
+      
     ],
   })
 );
