c93aad041245025309444bd48fbcc28c022df4bc
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-02-26 01:53:27 +0200

    Add client accepted student endpoint to request data

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.use(
       'http://localhost:3000',
       'http://127.0.0.1:5500',
       'https://rpms-admin.vercel.app',
+      'https://pupils.rulerspalacemontessori.com.ng',
+      'https://rpms-pupil.vercel.app',
     ],
   })
 );
