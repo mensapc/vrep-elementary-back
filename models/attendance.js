f23be89a61b66b0e8a17bd191e9c32147128d5d8
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2024-03-03 20:00:29 +0200

    modify attendance model

--- models/attendance.js
+++ models/attendance.js
@@ -1,21 +1,20 @@
 const mongoose = require('mongoose');
 
 const attendanceSchema = new mongoose.Schema({
-  academic_year: {
-    type: String,
+  score: {
+    type: Number,
+    required: true,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+  percentage: {
+    type: Number,
     required: true,
   },
   student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
   _class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true },
-  staff: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff' },
-  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
-  attendance_status: {
-    type: String,
-    enum: ['Present', 'Absent', 'Tardy', 'Excused'],
-    required: true,
-  },
-  reason: { type: String },
-  comments: { type: String },
 });
 
 const Attendance = mongoose.model('Attendance', attendanceSchema);
